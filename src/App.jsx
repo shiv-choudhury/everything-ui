2a73548688dd8d7f0ead44e65f1f8ea46cf6d372
Shiv Choudhury <shiv.choudhury@privacera.com>
2023-06-06 14:17:36 +0530

    UI- Remove unwanted imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import "./App.css";
 import Router from "./Router";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Router />
