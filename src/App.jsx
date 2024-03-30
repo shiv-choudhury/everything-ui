a8c9e48d116c0e70466b1d6995e09463e16f94f5
shiv <shivprasadchoudhary1999@gmail.com>
2024-03-31 00:28:54 +0530

    UI- Move all wrapper components from main file to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import React from "react";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
+
+import { ThemeProvider } from "@mui/material";
+
+import { UserContextProvider } from "./Context/AppContext.jsx";
 import Router from "./Routes/Router";
+import theme from "./Styles/theme.jsx";
 
 function App() {
   return (
     <>
-      <Router />
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Auth0Provider
+            domain="dev-pgi25t52e4kxj3sp.us.auth0.com"
+            clientId="josFtwXTDGkE17SiNI9KXL5f10Ru4KX9"
+            authorizationParams={{
+              redirect_uri: window.location.origin
+            }}
+          >
+            <UserContextProvider>
+              <Router />
+            </UserContextProvider>
+          </Auth0Provider>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
-import { ThemeProvider } from "@mui/material";
-import theme from "./Styles/theme.jsx";
-import { UserContextProvider } from "./Context/AppContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <ThemeProvider theme={theme}>
-    <Auth0Provider
-      domain="dev-pgi25t52e4kxj3sp.us.auth0.com"
-      clientId="josFtwXTDGkE17SiNI9KXL5f10Ru4KX9"
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
-    >
-      <UserContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </UserContextProvider>
-    </Auth0Provider>
-  </ThemeProvider>
-);
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
