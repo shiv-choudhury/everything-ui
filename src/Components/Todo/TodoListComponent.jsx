d0ebfd00e8e1a6bef1c1c9fce03a10aafe154192
shiv <shivprasadchoudhary1999@gmail.com>
2024-05-07 01:18:39 +0530

    UI- Todo list ui improvements

--- src/Components/Todo/TodoListComponent.jsx
+++ src/Components/Todo/TodoListComponent.jsx
@@ -11,6 +11,7 @@ import {
   TextField,
   Typography
 } from "@mui/material";
+import moment from "moment";
 
 function TodoListComponent() {
   const [todos, setTodos] = useState([]);
@@ -18,12 +19,25 @@ function TodoListComponent() {
   const [index, setIndex] = useState(null);
   const inputRef = useRef(null);
   const todoInputRef = useRef([]);
-
+  const currentDateTime = moment().format("hh:mm a, DD MMMM YYYY");
+  let dummyTodo = {
+    completed: false,
+    text: "Add your Todo",
+    editMode: false,
+    dateTime: currentDateTime
+  };
   useEffect(() => {
     if (index) {
       todoInputRef.current[index].focus();
     }
   }, [index]);
+
+  useEffect(() => {
+    let data = localStorage.getItem("todos");
+    if (JSON.parse(data).length) {
+      setTodos(JSON.parse(data));
+    }
+  }, []);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -31,17 +45,28 @@ function TodoListComponent() {
 
   const handleAddTodo = () => {
     if (inputValue) {
-      let todoObj = { completed: false, text: inputValue, editMode: false };
-      setTodos([...todos, todoObj]);
+      let todoObj = {
+        completed: false,
+        text: inputValue,
+        editMode: false,
+        dateTime: currentDateTime
+      };
+      let newTodo = [...todos, todoObj];
+      addUpdateTodos(newTodo);
       setInputValue("");
       inputRef.current.focus();
     }
   };
 
+  const addUpdateTodos = (newTodo) => {
+    setTodos(newTodo);
+    localStorage.setItem("todos", JSON.stringify(newTodo));
+  };
+
   const deleteTodo = (index) => {
     let newTodo = [...todos];
     newTodo.splice(index, 1);
-    setTodos(newTodo);
+    addUpdateTodos(newTodo);
   };
 
   const handleInputKeyDown = (e) => {
@@ -59,7 +84,7 @@ function TodoListComponent() {
   const editTodo = (i) => {
     let newTodo = [...todos];
     newTodo[i] = { ...newTodo[i], ...{ editMode: !todos[i].editMode } };
-    setTodos(newTodo);
+    addUpdateTodos(newTodo);
     setIndex(i);
     // console.log("todoInputRef", todoInputRef.current[i]);
     // todoInputRef.current[i].focus();
@@ -68,7 +93,7 @@ function TodoListComponent() {
   const handleCheck = (i) => {
     let newTodo = [...todos];
     newTodo[i] = { ...newTodo[i], ...{ completed: !todos[i].completed } };
-    setTodos(newTodo);
+    addUpdateTodos(newTodo);
   };
 
   const handleTodoChange = (e, i) => {
@@ -79,9 +104,10 @@ function TodoListComponent() {
 
   return (
     <>
-      <Box my={2}>
-        <Stack columnGap={2} direction="row" alignItems="center" mb={2}>
+      <Box my={2} sx={{ width: "80%" }}>
+        <Stack columnGap={2} direction="row" alignItems="top" mb={2}>
           <TextField
+            fullWidth
             size="small"
             ref={inputRef}
             value={inputValue}
@@ -93,44 +119,63 @@ function TodoListComponent() {
           </Button>
         </Stack>
         <Typography variant="h5">List</Typography>
-        {todos?.map((todo, index) => (
-          <Stack key={index} direction="row" alignItems="center" columnGap={2}>
-            <Checkbox
-              size="large"
-              checked={todo.completed}
-              onChange={() => handleCheck(index)}
-            />
-            <TextField
-              size="small"
-              ref={(ref) => (todoInputRef.current[index] = ref)}
-              disabled={todo.completed}
-              InputProps={{
-                readOnly: !todo.editMode
+        {todos.length ? (
+          todos?.map((todo, index) => (
+            <Stack
+              key={index}
+              direction="row"
+              alignItems="center"
+              columnGap={2}
+              style={{
+                marginTop: 20,
+                background: "#f0f0f0",
+                padding: 20,
+                borderRadius: 10
               }}
-              className={`${todo.completed && "success"}`}
-              type="text"
-              onChange={(e) => handleTodoChange(e, index)}
-              onKeyDown={(e) => handleTodoKeyDown(e, index)}
-              value={todo.text}
-            />
-            <Button
-              variant="contained"
-              disabled={todo.completed}
-              color={!todo.editMode ? "secondary" : "success"}
-              className={!todo.editMode ? "Edit" : "save-btn"}
-              onClick={() => editTodo(index)}
             >
-              {!todo.editMode ? <EditIcon /> : <SaveIcon />}
-            </Button>
-            <Button
-              variant="contained"
-              color="error"
-              onClick={() => deleteTodo(index)}
-            >
-              <DeleteIcon />
-            </Button>
-          </Stack>
-        ))}
+              <Checkbox
+                size="large"
+                checked={todo.completed}
+                onChange={() => handleCheck(index)}
+              />
+              <TextField
+                fullWidth
+                size="small"
+                ref={(ref) => (todoInputRef.current[index] = ref)}
+                disabled={todo.completed}
+                InputProps={{
+                  readOnly: !todo.editMode
+                }}
+                className={`${todo.completed && "success"} white`}
+                type="text"
+                onChange={(e) => handleTodoChange(e, index)}
+                onKeyDown={(e) => handleTodoKeyDown(e, index)}
+                value={todo.text}
+              />
+              <Button
+                variant="contained"
+                disabled={todo.completed}
+                color={!todo.editMode ? "secondary" : "success"}
+                className={!todo.editMode ? "Edit" : "save-btn"}
+                onClick={() => editTodo(index)}
+              >
+                {!todo.editMode ? <EditIcon /> : <SaveIcon />}
+              </Button>
+              <Button
+                variant="contained"
+                color="error"
+                onClick={() => deleteTodo(index)}
+              >
+                <DeleteIcon />
+              </Button>
+              {todo.dateTime && <Typography>{todo.dateTime}</Typography>}
+            </Stack>
+          ))
+        ) : (
+          <Typography sx={{ textAlign: "center" }} variant="h5" pt={2}>
+            No Todos
+          </Typography>
+        )}
       </Box>
     </>
   );
